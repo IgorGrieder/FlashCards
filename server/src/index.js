b34cabaea1c4aa52c4386a2d4c05f60fe251b6a7
Igor Grieder Melo <igorgrieder@gmail.com>
2024-11-18 22:10:08 -0300

    Resolved some connnections problems

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,16 @@
 require("dotenv/config");
+const connectDB = require("./database/config.js");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+connectDB();
+
 app.get("/", (req, res) => {
   return res.json("Ola Mundo!");
 });
+
 app.listen(PORT);
