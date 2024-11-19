d4b1db08be03b0bc873dde3c1b2da3ccab6e6289
Igor Grieder Melo <igorgrieder@gmail.com>
2024-11-19 20:45:59 -0300

    Finished the basics of modeling the MVP and started developing the routes itself

--- server/src/controllers/routes.js
+++ server/src/controllers/routes.js
@@ -1,8 +1,10 @@
 import cardRoutes from "./cardController.js";
+import logInRoutes from "./loginController.js";
 
 // Main function that will populate the main application with all the routes
 const setUpRoutes = (app) => {
   app.use(cardRoutes);
+  app.use(logInRoutes);
 };
 
 export default setUpRoutes;
