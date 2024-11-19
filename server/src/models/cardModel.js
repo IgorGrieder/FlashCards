d7e2e24e0bdad62a6e6d089158e6fc72b7cc3fdb
Igor Grieder Melo <igorgrieder@gmail.com>
2024-11-19 19:57:14 -0300

    Changes on the cardSchema

--- server/src/models/cardModel.js
+++ server/src/models/cardModel.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const cardSchema = mongoose.Schema({
   question: { type: String, required: true },
+  category: { type: String },
+  img: { type: String },
 });
 
 export default cardSchema;
