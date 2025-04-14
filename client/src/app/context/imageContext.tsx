b4b510e08c74dd1549dd15198dba531c4585e550
Igor Grieder Melo <igorgrieder@gmail.com>
2025-04-14 19:20:21 -0300

    Tabs

--- client/src/app/context/imageContext.tsx
+++ client/src/app/context/imageContext.tsx
@@ -1,28 +1,28 @@
-import { createContext, MutableRefObject, useRef  } from "react";
+import { createContext, MutableRefObject, useRef } from "react";
 
 type ImageCaching = {
-    cache: MutableRefObject<string[]>;
-    revokeCache: VoidFunction;
-    insertCache: (url: string) => void;
+  cache: MutableRefObject<string[]>;
+  revokeCache: VoidFunction;
+  insertCache: (url: string) => void;
 }
 
 export const ImageContext = createContext<ImageCaching | null>(null);
 
-export function ImageContextProvider({children}: {children: React.ReactNode}) {
-    const cache = useRef<string[]>([]);
+export function ImageContextProvider({ children }: { children: React.ReactNode }) {
+  const cache = useRef<string[]>([]);
 
-    const revokeCache = (): void => {
-        for (const url in cache.current) {
-            URL.revokeObjectURL(url);
-        }
+  const revokeCache = (): void => {
+    for (const url in cache.current) {
+      URL.revokeObjectURL(url);
     }
+  }
 
-    const insertCache = (url: string): void => {
-        cache.current.push(url);
-    }
-    return (
-        <ImageContext.Provider value={{ cache, revokeCache, insertCache }}>
-            {children}
-        </ImageContext.Provider>
-    )
+  const insertCache = (url: string): void => {
+    cache.current.push(url);
+  }
+  return (
+    <ImageContext.Provider value={{ cache, revokeCache, insertCache }}>
+      {children}
+    </ImageContext.Provider>
+  )
 }
